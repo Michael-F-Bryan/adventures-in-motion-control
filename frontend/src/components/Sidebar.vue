746cf4b260bc185ac10eb6c15715a6c5be5ede42
Michael Bryan <michaelfbryan@gmail.com>
2019-09-27 01:25:15 +0800

    Deleted the "Funny Business" sidebar section

--- frontend/src/components/Sidebar.vue
+++ frontend/src/components/Sidebar.vue
@@ -42,17 +42,6 @@
         </b-card-body>
       </b-collapse>
     </b-card>
-
-    <b-card no-body class="mb-1">
-      <b-card-header header-tag="header" class="p-1" role="tab">
-        <b-button block href="#" v-b-toggle.accordion-3 variant="info">Funny Business</b-button>
-      </b-card-header>
-      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
-        <b-card-body>
-          <b-card-text>This is where you can inject errors and things</b-card-text>
-        </b-card-body>
-      </b-collapse>
-    </b-card>
   </div>
 </template>
 
