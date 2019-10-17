5d901ce49e52cec981495ad4b06ee6f6a428efaa
Michael Bryan <michaelfbryan@gmail.com>
2019-10-17 23:49:19 +0800

    Refactoring and added more tests

--- motion/src/movements/translator.rs
+++ motion/src/movements/translator.rs
@@ -55,21 +55,22 @@ impl Translator {
         match command.major_number() {
             0 | 1 => self.handle_linear_interpolate(command, cb),
             2 | 3 => self.handle_circular_interpolate(command, cb),
-
-            4 => match command.value_for('P') {
-                Some(dwell_time) => {
-                    cb.dwell(Duration::from_secs_f32(dwell_time))
-                },
-                None => {
-                    cb.invalid_argument(command, 'P', "Dwell time not provided")
-                },
-            },
+            4 => self.handle_dwell(command, cb),
 
             20 => self.units = Units::Inches,
             21 => self.units = Units::Millimetres,
             90 => self.coordinate_mode = CoordinateMode::Absolute,
             91 => self.coordinate_mode = CoordinateMode::Relative,
             _ => cb.unsupported_command(command),
+        }
+    }
+
+    fn handle_dwell<C: Callbacks>(&mut self, command: &GCode, mut cb: C) {
+        match command.value_for('P') {
+            Some(dwell_time) => cb.dwell(Duration::from_secs_f32(dwell_time)),
+            None => {
+                cb.invalid_argument(command, 'P', "Dwell time not provided")
+            },
         }
     }
 
@@ -121,9 +122,8 @@ impl Translator {
 
         // TODO: Take the plane into account (G17, G18, G19)
         Ok(Point {
-            x: self.to_length(x),
-            y: self.to_length(y),
             z: self.current_location.z,
+            ..self.absolute_location(x, y, 0.0)
         })
     }
 
@@ -146,13 +146,6 @@ impl Translator {
         let y = command.value_for('Y').unwrap_or(0.0);
         let z = command.value_for('Z').unwrap_or(0.0);
         self.absolute_location(x, y, z)
-    }
-
-    fn to_length(&self, raw: f32) -> Length {
-        match self.units {
-            Units::Millimetres => Length::new::<millimeter>(raw),
-            Units::Inches => Length::new::<inch>(raw),
-        }
     }
 
     fn absolute_location(&self, x: f32, y: f32, z: f32) -> Point {
@@ -271,7 +264,11 @@ mod tests {
 
     #[derive(Debug, Default)]
     struct MockCallbacks {
-        linear_interp: Vec<(Point, Point, Velocity)>,
+        linear: Vec<(Point, Point, Velocity)>,
+        circular: Vec<(Point, Point, Point, Direction, Velocity)>,
+        dwell: Vec<Duration>,
+        unknown_commands: u32,
+        invalid: Vec<char>,
     }
 
     impl Callbacks for MockCallbacks {
@@ -281,12 +278,41 @@ mod tests {
             end: Point,
             feed_rate: Velocity,
         ) {
-            self.linear_interp.push((start, end, feed_rate));
+            self.linear.push((start, end, feed_rate));
+        }
+
+        fn circular_interpolate(
+            &mut self,
+            start: Point,
+            centre: Point,
+            end: Point,
+            direction: Direction,
+            feed_rate: Velocity,
+        ) {
+            self.circular
+                .push((start, centre, end, direction, feed_rate));
+        }
+
+        fn dwell(&mut self, duration: Duration) { self.dwell.push(duration); }
+
+        fn unsupported_command(&mut self, _command: &GCode) {
+            self.unknown_commands += 1;
+        }
+
+        fn invalid_argument(
+            &mut self,
+            _command: &GCode,
+            arg: char,
+            _reason: &'static str,
+        ) {
+            self.invalid.push(arg);
         }
     }
 
     fn parse(src: &str) -> GCode {
-        let line = gcode::parse(src).next().unwrap();
+        let lines: Vec<_> = gcode::parse(src).collect();
+        assert_eq!(lines.len(), 1);
+        let line = &lines[0];
         assert!(line.comments().is_empty());
         let commands = line.gcodes();
         assert_eq!(commands.len(), 1);
@@ -302,13 +328,158 @@ mod tests {
 
         trans.translate(&gcode, &mut mocks);
 
-        assert_eq!(mocks.linear_interp.len(), 1);
-        let got = mocks.linear_interp[0];
+        assert_eq!(mocks.linear.len(), 1);
+        let got = mocks.linear[0];
         let expected_end = Point::new::<millimeter>(50.0, 10.0, -5.0);
         let expected_feed = Velocity::new::<millimeter_per_minute>(1000.0);
         assert_eq!(got, (start, expected_end, expected_feed));
 
         assert_eq!(trans.current_location, expected_end);
         assert_eq!(trans.feed_rate, expected_feed);
+    }
+
+    #[test]
+    fn more_complicated_relative_circular_interpolation() {
+        let mut trans = Translator {
+            current_location: Point::new::<inch>(1.0, 10.0, -50.0),
+            coordinate_mode: CoordinateMode::Relative,
+            units: Units::Inches,
+            ..Default::default()
+        };
+        let start = trans.current_location;
+        let gcode = parse("G02 X10 Y-5 I5 J90 F1000.0");
+        let mut mocks = MockCallbacks::default();
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(mocks.circular.len(), 1);
+        let (got_start, got_centre, got_end, got_direction, got_feed) =
+            mocks.circular[0];
+        assert_eq!(got_start, start);
+        assert_eq!(got_direction, Direction::Clockwise);
+        let expected_end = start + Point::new::<inch>(10.0, -5.0, 0.0);
+        assert_eq!(got_end, expected_end);
+        let expected_centre = start + Point::new::<inch>(5.0, 90.0, 0.0);
+        assert_eq!(got_centre, expected_centre);
+        let expected_feed =
+            Length::new::<inch>(1000.0) / Time::new::<minute>(1.0);
+        assert_eq!(got_feed, expected_feed);
+
+        assert_eq!(trans.current_location, expected_end);
+        assert_eq!(trans.feed_rate, expected_feed);
+    }
+
+    #[test]
+    fn switch_to_inches() {
+        let mut trans = Translator {
+            units: Units::Millimetres,
+            ..Default::default()
+        };
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G20");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(trans.units, Units::Inches);
+    }
+
+    #[test]
+    fn switch_to_millimeters() {
+        let mut trans = Translator {
+            units: Units::Inches,
+            ..Default::default()
+        };
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G21");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(trans.units, Units::Millimetres);
+    }
+
+    #[test]
+    fn switch_to_absolute() {
+        let mut trans = Translator {
+            coordinate_mode: CoordinateMode::Relative,
+            ..Default::default()
+        };
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G90");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(trans.coordinate_mode, CoordinateMode::Absolute);
+    }
+
+    #[test]
+    fn switch_to_relative() {
+        let mut trans = Translator {
+            coordinate_mode: CoordinateMode::Absolute,
+            ..Default::default()
+        };
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G91");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(trans.coordinate_mode, CoordinateMode::Relative);
+    }
+
+    #[test]
+    fn dwell() {
+        let mut trans = Translator::default();
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G04 P1.0");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(mocks.dwell.len(), 1);
+        assert_eq!(mocks.dwell[0], Duration::from_secs(1));
+    }
+
+    #[test]
+    fn dwell_period_is_required() {
+        let mut trans = Translator::default();
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G04");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert!(mocks.dwell.is_empty());
+        assert_eq!(1, mocks.invalid.len());
+        assert_eq!('P', mocks.invalid[0]);
+    }
+
+    #[test]
+    fn unsupported_g_command() {
+        let mut trans = Translator::default();
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("G42");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(1, mocks.unknown_commands);
+    }
+
+    #[test]
+    fn unsupported_m_command() {
+        let mut trans = Translator::default();
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("M42");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(1, mocks.unknown_commands);
+    }
+
+    #[test]
+    fn unsupported_mnemonic() {
+        let mut trans = Translator::default();
+        let mut mocks = MockCallbacks::default();
+        let gcode = parse("T42");
+
+        trans.translate(&gcode, &mut mocks);
+
+        assert_eq!(1, mocks.unknown_commands);
     }
 }
